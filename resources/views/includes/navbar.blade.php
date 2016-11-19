0907a51f3189aa4708cd22caf3617c45a9e37e22
Bebrazmogis <justas963@gmail.com>
2016-11-19 23:14:45 +0100

    Navbaras, visi css,bootstrap link perkelti į atskirą failą

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="nav navbar pull-right">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="{{ route('home') }}">Pradžia</a></li>
+                <li><a href="#">Paslaugos</a></li>
+                @if(Auth::check())
+                <li><a href="{{ route('user.index') }}">Mano Meniu</a></li>
+                <li><a href="{{ route('auth.logout') }}">Atsijungti</a></li>
+                @else 
+                <li><a href="{{ route('auth.register') }}">Registracija</a></li>
+                <li><a href="{{ route('auth.login') }}">Prisijungimas</a></li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,20 +1,12 @@
 <!DOCTYPE html>
 <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    
-    <link href="{{ elixir("css/app.css") }}" rel="stylesheet" data-turbolinks-track="reload">
+    @include('includes.dependencies')
 
     <title>@yield('title')</title>
 </head>
 <body>
 <div class="container">
+    @include('includes.navbar')
     <div class="messages row">
         @include('includes.messages')
     </div>
