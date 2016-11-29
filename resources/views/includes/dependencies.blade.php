dcb7879467e90558024bdf580e45884732cbb314
Bebrazmogis <justas963@gmail.com>
2016-11-29 21:43:32 +0100

    View stuff, csrf tokenas js

--- resources/views/includes/dependencies.blade.php
+++ resources/views/includes/dependencies.blade.php
@@ -1,10 +1,9 @@
+
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-<link href="{{ elixir("css/app.css") }}" rel="stylesheet" data-turbolinks-track="reload">
+<link href='{{ elixir("css/app.css") }}' rel="stylesheet" data-turbolinks-track="reload">
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <head>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <script>
+        window.Laravel = { csrfToken: '{{ csrf_token() }}' };
+    </script>
     @include('includes.dependencies')
 
     <title>@yield('title')</title>
@@ -11,5 +15,7 @@
         @include('includes.messages')
     </div>
     @yield('content');
+
+    @include('includes.javascript')
 </div>
 </body>
