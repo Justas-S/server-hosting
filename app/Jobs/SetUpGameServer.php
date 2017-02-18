f569e5918f0e46bc19419ccf7e63ff94f57d8804
Bebrazmogis <justas963@gmail.com>
2017-02-18 02:05:36 +0100

    Į serverio paruošimą nusipirkus dabar įeina ir default server_package instaliavimas

--- app/Jobs/SetUpGameServer.php
+++ app/Jobs/SetUpGameServer.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Services\DatabaseGenerator;
 use App\Services\ServerManager;
+use App\Services\GameServerManager;
 use App\GameServer;
 use App\Events\GameServerSetUpEvent;
 
@@ -23,16 +24,19 @@ class SetUpGameServer implements ShouldQueue
     protected $servermanager;
 
     protected $dbgenerator;
+
+    protected $gameservermanager;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(GameServer $gameserver, ServerManager $server_manager, DatabaseGenerator $generator)
+    public function __construct(GameServer $gameserver, ServerManager $server_manager, DatabaseGenerator $generator, GameServerManager $gameservermanager)
     {
         $this->gameserver = $gameserver;
         $this->servermanager = $server_manager;
         $this->dbgenerator = $generator;
+        $this->gameservermanager = $gameservermanager;
     }
 
     /**
@@ -60,7 +64,8 @@ class SetUpGameServer implements ShouldQueue
 
         if ($this->servermanager->createDatabase($server, $database->name)
             && $this->servermanager->createDatabaseUser($server, $dbuser->username, $dbuser->password, $dbuser->database->name) 
-            && $this->servermanager->createFtpUser($server, $ftpuser->username, $ftpuser->password)) {
+            && $this->servermanager->createFtpUser($server, $ftpuser->username, $ftpuser->password)
+            && $this->gameservermanager->installServerPackage($gameserver, $ftpuser->username, $gameserver->game->default_server_package)) {
             event(new GameServerSetUpEvent($this->gameserver, $database, $dbuser, $ftp_user));
         } else {
             // TODO ERROR
